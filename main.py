10380267b5bcca17953460aa1f9244542f8f1009
zjmarg <76889896+zjmarg@users.noreply.github.com>
2023-09-21 19:37:12 -0400

    edited initDatabase

--- main.py
+++ main.py
@@ -3,15 +3,9 @@ import sqlite3
 # filename to form database
 DB_FILENAME = "planner.db"
 
+# function that initializes the db
 def initDatabase():
     conn = sqlite3.connect(DB_FILENAME)
-
-    conn.execute('''CREATE TABLE COMPANY
-             (ID INT PRIMARY KEY     NOT NULL,
-             NAME           TEXT    NOT NULL,
-             AGE            INT     NOT NULL,
-             ADDRESS        CHAR(50),
-             SALARY         REAL);''')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':

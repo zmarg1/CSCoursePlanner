6a21dc4a56b9d4225c66c0c4fe3de61e8cbcfd42
zjmarg <76889896+zjmarg@users.noreply.github.com>
2023-10-22 19:07:28 -0400

    added more data

--- data/data_scraping.py
+++ data/data_scraping.py
@@ -1,50 +1,55 @@
 import pandas as pd
+import math
 
-def csv_to_excel():
-    # Load the original CSV file (modify the file path as needed)
-    original_df = pd.read_csv("CSEE_Past_Course_Frequency.csv")
+# Load the original CSV file (modify the file path as needed)
+original_df = pd.read_csv("CSEE_Past_Course_Frequency.csv")
 
-   # Create a list of columns representing semester and year
-    semesters_and_years = original_df.columns[3:]
+# Create a list of columns representing semester and year
+semesters_and_years = original_df.columns[3:]
 
-    # Initialize lists to store the data for the new DataFrame
-    class_code_list = []
-    class_number_list = []
-    semester_list = []
-    year_list = []
-    frequency_count_list = []
+# Initialize lists to store the data for the new DataFrame
+subject_code_list = []
+class_number_list = []
+term_list = []  # Changed the column name to "term"
+year_list = []
+frequency_count_list = []
 
-    # Iterate through the original DataFrame to transform the data
-    for _, row in original_df.iterrows():
-        class_code = row[0]  # Assuming class_code is in the first column
-        class_number = row[1]  # Assuming class_number is in the second column
-        
-        for semester_year, frequency in zip(semesters_and_years, row[3:]):
-            if pd.notna(frequency):
+# Iterate through the original DataFrame to transform the data
+for _, row in original_df.iterrows():
+    subject_code = row[0]  # Assuming subject_code is in the first column
+    class_number = row[1]  # Assuming class_number is in the second column
+    
+    for semester_year, frequency in zip(semesters_and_years, row[3:]):
+        if pd.notna(frequency):
+            semester, year = semester_year.split()
+            year = str(math.floor(float(year)))  # Round down the year to remove decimals
+            subject_code_list.append(subject_code)
+            class_number_list.append(class_number)
+            term_list.append(semester)  # Changed the column name to "term"
+            year_list.append(year)
+            frequency_count_list.append(frequency)
+        else:
+            # Append empty values for missing frequencies
+            subject_code_list.append(subject_code)
+            class_number_list.append(class_number)
+            if " " in semester_year:
                 semester, year = semester_year.split()
-                class_code_list.append(class_code)
-                class_number_list.append(class_number)
-                semester_list.append(semester)
+                year = str(math.floor(float(year)))  # Round down the year to remove decimals
+                term_list.append(semester)  # Changed the column name to "term"
                 year_list.append(year)
-                frequency_count_list.append(frequency)
             else:
-                # Append empty values for missing frequencies
-                class_code_list.append(class_code)
-                class_number_list.append(class_number)
-                semester_list.append(semester_year)
+                term_list.append(semester_year)  # Changed the column name to "term"
                 year_list.append("")
-                frequency_count_list.append("")
+            frequency_count_list.append("")
 
-    # Create a new DataFrame with the transformed data
-    new_df = pd.DataFrame({
-        "class_code": class_code_list,
-        "class_number": class_number_list,
-        "semester": semester_list,
-        "year": year_list,
-        "frequency_count": frequency_count_list
-    })
+# Create a new DataFrame with the transformed data
+transformed_df = pd.DataFrame({
+    "subject_code": subject_code_list,
+    "class_number": class_number_list,
+    "term": term_list,  # Changed the column name to "term"
+    "year": year_list,
+    "frequency_count": frequency_count_list
+})
 
-    # Write the new DataFrame to an Excel file
-    new_df.to_excel("transformed_data.xlsx", index=False)
-if __name__ == "__main__":
-    csv_to_excel()
+# Write the final transformed data to an Excel file
+transformed_df.to_excel("final_transformed_data.xlsx", index=False)

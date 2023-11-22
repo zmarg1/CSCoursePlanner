7fb55e51981eacd9dc8c36e64e3177fd66665beb
AmirStew101 <amirstew@gmail.com>
2023-11-22 10:27:14 -0500

    fixed warning about reactDOM in inspect

--- FrontEnd/src/index.tsx
+++ FrontEnd/src/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './app'; // Import App from app.tsx
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElm =  document.getElementById('root');
+
+if (rootElm){
+  const root = ReactDOM.createRoot(rootElm);
+
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+else{
+  console.error('Root element with id "root" not found');
+}
